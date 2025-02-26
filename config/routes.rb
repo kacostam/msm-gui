6b5085103ed21be66ff4a0f4447b0b64f0f49394
Karla Acosta <77418536+kacostam@users.noreply.github.com>
2025-02-26 22:22:08 +0000

    create new director

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,22 @@ Rails.application.routes.draw do
   get("/directors/youngest", { :controller => "directors", :action => "max_dob" })
   get("/directors/eldest", { :controller => "directors", :action => "min_dob" })
 
+
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/:path_id", { :controller => "directors", :action => "show" })
+
+  post("/insert_director_record", {:controller => "directors", :action => "create" })
+  get("/delete_director/:a_director_id", {:controller => "directors", :action => "destroy" })
+  post("/update_director_record/:a_director_id", {:controller => "directors", :action => "update" })
+
 
   get("/movies", { :controller => "movies", :action => "index" })
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
 
-
   post("/insert_movie_record", {:controller => "movies", :action => "create" })
   get("/delete_movie/:a_movie_id", {:controller => "movies", :action => "destroy" })
   post("/update_movie_record/:a_movie_id", {:controller => "movies", :action => "update" })
+
 
   get("/actors", { :controller => "actors", :action => "index" })
   get("/actors/:path_id", { :controller => "actors", :action => "show" })
